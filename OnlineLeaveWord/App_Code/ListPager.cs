55b8d829a39fcb75bc213ea919cdd5106d1c3bd7
liutianping <liutianping4558358@gmail.com>
2011-11-28 01:28:28 +0800

    modified for add page

---
+++ OnlineLeaveWord/App_Code/ListPager.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Data;
+using System.Configuration;
+using System.Collections.Generic;
+
+/// <summary>
+/// ListPager 的摘要说明
+/// </summary>
+public class ListPager<T> : System.Collections.Generic.List<T>
+{
+    public ListPager()
+    {
+        //
+        // TODO: 在此处添加构造函数逻辑
+        //
+    }
+
+    private int _CurrentIndex;
+    private int _PageSize;
+    private int _TotalItem;
+    private int _PageCount;
+
+    /// <summary>
+    /// 当前页码
+    /// </summary>
+    public int CurrentIndex
+    {
+        get { return _CurrentIndex; }
+        set { _CurrentIndex = value; }
+    }
+
+    /// <summary>
+    /// 每页大小
+    /// </summary>
+    public int PageSize
+    {
+        get { return _PageSize; }
+        set { _PageSize = value; }
+    }
+
+    /// <summary>
+    /// 记录总数
+    /// </summary>
+    public int TotalItem
+    {
+        get { return _TotalItem; }
+        set { _TotalItem = value; }
+    }
+
+    /// <summary>
+    /// 页面总数
+    /// </summary>
+    public int PageCount
+    {
+        get { return _PageCount; }
+        set { _PageCount = value; }
+    }
+
+    /// <summary>
+    /// 构造函数
+    /// </summary>
+    /// <param name="list">要分页的List泛型</param>
+    /// <param name="index">起始页码</param>
+    /// <param name="pageSize">分页大小</param>
+    public ListPager(List<T> list, int index, int pageSize)
+    {
+        this._CurrentIndex = index;
+        this._PageSize = pageSize;
+
+        int startIndex = (this._CurrentIndex - 1) * PageSize;
+        for (int i = startIndex; i < startIndex + this._PageSize && i < list.Count; i++)
+        {
+            this.Add(list[i]);
+        }
+
+        this._TotalItem = list.Count;
+        //this._PageCount = (this._TotalItem / PageSize) + 1;
+        this.PageCount = (this._TotalItem + this.PageSize - 1) / this._PageSize;
+    }
+}
--- OnlineLeaveWord/Index.aspx.cs
+++ OnlineLeaveWord/Index.aspx.cs
@@ -15,22 +15,39 @@ using System.Text;
 
 public partial class Index : System.Web.UI.Page
 {
+    private static int currentIndex = 1;
+    private static int pageSize = int.Parse(ConfigurationManager.AppSettings["pageSize"].ToString());
+    private static ListPager<OnlineLeaveWord.Model.LeaveWord_M> list = null;
     protected void Page_Load(object sender, EventArgs e)
     {
         if (!Page.IsPostBack)
         {
-            bindDlLeaveWord();
+            bindDlLeaveWord(currentIndex,pageSize);
         }
     }
 
-    private void bindDlLeaveWord()
+    private void bindDlLeaveWord(int currIndex,int pagesize)
     {
         OnlineLeaveWord.BLL.LeaveWordImpl.LeaveWord lw = new OnlineLeaveWord.BLL.LeaveWordImpl.LeaveWord();
         UserInfo_M u = new UserInfo_M();
-        List<OnlineLeaveWord.Model.LeaveWord_M> list = (List<OnlineLeaveWord.Model.LeaveWord_M>)lw.GetAllLeaveWord(u);
-        Repeater1.DataSource = list;
+        list = new ListPager<OnlineLeaveWord.Model.LeaveWord_M>((List<OnlineLeaveWord.Model.LeaveWord_M>)lw.GetAllLeaveWord(u), currIndex, pagesize);
+        Repeater1.DataSource = list;    
         Repeater1.DataBind();
+        BindPagerControls();
+        
     }
+
+    private void BindPagerControls()
+    {
+        // 判断上一页，下一页按钮是否启用.
+        btnPrevious.Enabled = currentIndex != 1;
+        btnNext.Enabled = currentIndex != list.PageCount;
+
+        lblCurrentpage.Text = list.CurrentIndex.ToString();
+        lblTotalPage.Text = list.PageCount.ToString();
+        lblPageinfo.Text = String.Format("共{0}条记录，每页显示{1}条", list.TotalItem, list.PageSize);
+    }
+
 
     public string SubString(string str,int length)
     {
@@ -40,39 +57,30 @@ public partial class Index : System.Web.UI.Page
             return str;
     }
 
-    //public string GetContent(Object item)
-    //{
-    //    LeaveWord_M lw = (LeaveWord_M)item;
-    //    int articleID = lw.Id;
-    //    int replyCount = 0;
-    //    string pageViews = "1";
-    //    OnlineLeaveWord.BLL.ReplyImpl.ReplyImp reply = new OnlineLeaveWord.BLL.ReplyImpl.ReplyImp();
-    //    replyCount = reply.GetReplyCountByLeaveWordId(lw);
-    //    pageViews=lw.PageViews == "0" ? "1" : lw.PageViews;
-    //    if (lw.Subject.Length > 15)
-    //    {
-    //        lw.Subject = lw.Subject.Substring(0,10)+"...";
-    //    }
-    //    if (lw.Content.Length > 20)
-    //    {
-    //        lw.Content = lw.Content.Substring(0,15)+"...";
-    //    }
-    //    StringBuilder sb = new StringBuilder();
-    //    sb.Append("<span style='text-align:center;margin-left: 10px;'>");
-    //    sb.Append(lw.Subject);
-    //    sb.Append("</span>");
-    //    sb.Append("<span style='text-align:left'>");
-    //    sb.Append("&nbsp;&nbsp;&nbsp;&nbsp;");
-    //    sb.Append(lw.Content);
-    //    sb.Append("</span>");
-    //    sb.Append("&nbsp;&nbsp;&nbsp;&nbsp;");
-    //    sb.Append("评论(<a href=LeaveWordDeatil.aspx?articleid=");
-    //    sb.Append(lw.Id.ToString());
-    //    sb.Append(">"+replyCount.ToString());
-    //    sb.Append("</a>");
-    //    sb.Append("|浏览(");
-    //    sb.Append(pageViews);
-    //    sb.Append(")");
-    //    return sb.ToString();
-    //}
+    protected void btnfirstpage_click(object sender, EventArgs e)
+    {
+        currentIndex = 1; //修正为1，第一次发的时候写成0了，2B了～
+        bindDlLeaveWord(currentIndex, pageSize);
+        BindPagerControls();
+    }
+    protected void btnprevious_click(object sender, EventArgs e)
+    {
+        --currentIndex;
+        bindDlLeaveWord(currentIndex, pageSize);
+        BindPagerControls();
+        
+    }
+    protected void btnnext_click(object sender, EventArgs e)
+    {
+        ++currentIndex;
+        bindDlLeaveWord(currentIndex, pageSize);
+        BindPagerControls();
+        
+    }
+    protected void btnlastpage_click(object sender, EventArgs e)
+    {
+        currentIndex = list.PageCount;
+        bindDlLeaveWord(currentIndex, pageSize);
+        BindPagerControls();
+    }
 }
